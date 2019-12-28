7a46a085e2832c08b2c7adb20c16cce59def3a53
karalius32 <59309454+karalius32@users.noreply.github.com>
2019-12-28 13:33:22 +0200

    Add files via upload

---
+++ Unity2DHelperScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MyMethods
+{
+    public class RotationMethods
+    {
+        public static void RotateToObjectInZaxis(GameObject rotatingGameObject, Vector3 target, float addModifier)
+        {
+            float angle = Mathf.Atan2(target.y - rotatingGameObject.transform.position.y,
+                target.x - rotatingGameObject.transform.position.x) * Mathf.Rad2Deg;
+            rotatingGameObject.transform.rotation = Quaternion.Euler(0, 0, angle + addModifier);
+        }
+
+        public static void Rotate2DObjectBasedOnItsVelocity(Rigidbody2D rotatingGameObjectRB, float addModifier)
+        {
+            var angle = Mathf.Atan2(rotatingGameObjectRB.velocity.y, rotatingGameObjectRB.velocity.x) * Mathf.Rad2Deg;
+            rotatingGameObjectRB.MoveRotation(angle + addModifier);
+        }
+
+        public static void RotateToMousePos(GameObject rotatingGameObject, Camera camera, float addModifier)
+        {
+            var mousePos = InputMethods.GetMousePos(camera);
+            RotateToObjectInZaxis(rotatingGameObject, mousePos, addModifier);
+        }
+    }
+
+    public class InputMethods
+    {
+        public static Vector3 GetMousePos(Camera camera)
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = -camera.transform.position.z;
+            return camera.ScreenToWorldPoint(mousePos);
+        }
+    }
+}
+
